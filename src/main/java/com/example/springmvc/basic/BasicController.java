72b50dd16e6e2b17011e60a23131741aa8b50520
이현욱 <lhw751@gmail.com>
2026-02-23 09:54:08 +0900

    타임리프 10강 - URL 링크 소스코드 추가

--- src/main/java/com/example/springmvc/basic/BasicController.java
+++ src/main/java/com/example/springmvc/basic/BasicController.java
@@ -58,6 +58,13 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
